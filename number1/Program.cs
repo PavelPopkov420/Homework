0c9281fe2e06428c093615011c11a9d3f86c7568
Pavel Popkov <goil2011@yandex.ru>
2022-07-12 22:23:49 +0300

    number1

---
+++ number1/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.WriteLine("Первое число больше: " + Convert.ToString(a));
+else 
+    Console.WriteLine("Второе чисто больше: " + Convert.ToString(b));
