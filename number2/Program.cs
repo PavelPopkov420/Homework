8b998d18271c2d1aa9629fe2953220b60869689c
Pavel Popkov <goil2011@yandex.ru>
2022-07-12 23:09:06 +0300

    number2

---
+++ number2/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+    Console.WriteLine("Максимальное число: " + Convert.ToString(a));
+
+if (b > a && b > c)
+   Console.WriteLine("Максимальное число: " + Convert.ToString(b));
+
+if (c > b && c > a)
+    Console.WriteLine("Максимальное число: " + Convert.ToString(c));
+
