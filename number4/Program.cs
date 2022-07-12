13794bb3b0087f2cd60c9d163b551e815ea0a061
Pavel Popkov <goil2011@yandex.ru>
2022-07-13 00:17:54 +0300

    number4

---
+++ number4/Program.cs
@@ -0,0 +1,15 @@
+﻿int N = Convert.ToInt32(Console.ReadLine());
+int i = 1, k = 1;
+while (true)
+{
+	if (i % 2 == 0)
+	{
+		Console.Write(i + " ");
+		k++;
+	}
+	if (k > N)
+	{
+		break;
+	}
+	i++;
+}
